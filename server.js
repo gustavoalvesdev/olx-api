294135c015031c5c8376a92660f8a229a808b814
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2021-10-27 02:14:47 -0300

    Criando as rotas

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const fileupload = require('express-fileupload');
 
+const apiRoutes = require('./src/routes');
+
 mongoose.connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useFindAndModify: false,
@@ -24,9 +26,7 @@ server.use(fileupload());
 
 server.use(express.static(__dirname + '/public'));
 
-server.get('/ping', (req, res) => {
-    res.json({pong: true});
-})
+server.use('/', apiRoutes)
 
 server.listen(process.env.PORT, () => {
     console.log(`- Rodando no Endereço: ${process.env.BASE}`)
